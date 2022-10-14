1d7c0ecc17faf98dc410464bd73eb4f553145026
OGray96 <ogray54@gmail.com>
2022-10-15 10:42:28 +1100

    Adding contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import '../styles/contact.css'
 
-export default function Home() {
-    return (
-      <div>
-        <h1>Contact Me</h1>
-        <p>
-         Contact Form coming soon!
-        </p>
-      </div>
-    );
+export default function BasicExample() {
+  return (
+    <Form>
+      <Form.Group className="mb-3 email" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3 user-name" controlId="formBasicText">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter name" />
+      </Form.Group>
+      <Form.Group className="mb-3 text-area" controlId="formBasicText">
+        <Form.Label>Message</Form.Label>
+        <Form.Control as ='textarea'className ='text' label="Check me out" />
+      </Form.Group>
+      <Button className = 'button' variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
