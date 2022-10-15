5ff2c4b2185dac1e1f974fe6870bc4445212e7b6
OGray96 <ogray54@gmail.com>
2022-10-15 11:42:41 +1100

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './styles/footer.css'
+import githubLogo from './images/github-logo.png'
+import linkedinLogo from './images/linkedin-logo.png'
 
 export default function Footer() {
     return (
       <div className='footer-container'>
-        <p>
-         All rights reserved by Oliver Gray 2022
-        </p>
+        <div className='image-container'>
+          <a href='https://github.com/OGray96' >
+            <img className = 'images' src = {githubLogo} alt='GitHub Logo'/>
+          </a>
+          <a href='https://www.linkedin.com/in/oliver-gray1/' >
+          <img className = 'images' src = {linkedinLogo} alt='GitHub Logo'/>
+          </a>
+        </div>
       </div>
     );
   }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,20 +5,16 @@ import validateEmail from '../../utils/emailValidation.js';
 
 
 function Contact() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
   const [email, setEmail] = useState('');
   const [userName, setUserName] = useState('');
   const [userMessage, setUserMessage] = useState('')
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'userName') {
@@ -29,15 +25,16 @@ function Contact() {
   };
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
+    if (!validateEmail(email)) {
+      setErrorMessage('Please enter a valid email address');
       return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+
+    if (!userName) {
+      setErrorMessage('Oops looks like you forgot to write a name!');
+      return;
     }
     if (!userMessage) {
       setErrorMessage(
@@ -45,9 +42,8 @@ function Contact() {
       );
       return;
     }
-    alert(`Hello ${userName}`);
+    alert(`Message submitted, thanks ${userName}! Oliver will be in contact soon!`);
 
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
     setUserName('');
     setUserMessage('');
     setEmail('');
